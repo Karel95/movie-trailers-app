c4dce46fe8bd889f83d851b28a22a520f7fca119
Karel <karelh2207@gmail.com>
2024-09-24 14:38:28 -0300

    actualizando

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -64,11 +64,13 @@ const MovieDetailCarrusel = ({
                       height: "100%",
                       playerVars: {
                         autoplay: 1,
-                        controls: 1, // Cambia a 1 si deseas mostrar los controles
+                        controls: 1,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
                         rel: 0,
-                        modestbranding: 1,
                         showinfo: 0,
-                        enablejsapi: 1, // Habilitar la API de JavaScript
                       },
                     }}
                   />
